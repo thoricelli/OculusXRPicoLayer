f24af065d4992ab919218ea7d3d09fcde77ddab7
thoricelli <roanheylen@hotmail.com>
2024-06-04 18:42:29 +0200

    Screwed up the compile script (I'm stupid)

--- compile_linux.bash
+++ compile_linux.bash
@@ -2,6 +2,6 @@
 clang="aarch64-linux-android30-clang"
 
 mkdir -p out
-$clang -fdeclspec -gdwarf -fPIC -shared -o libPxrPlatform.so libpxr_api.so OculusXRPlugin.c
+$clang -fdeclspec -gdwarf -fPIC -shared -o out/libOculusXRPlugin.so libPxrPlatform.so libpxr_api.so OculusXRPlugin.c
 $clang -fdeclspec -gdwarf -fPIC -shared -o out/libovrplatformloader.so libpxrplatformloader.so libpxr_api.so OvrPlatformLoader.c
-$clang -fdeclspec -gdwarf -fPIC -shared -o libpxr_api.so libPxrPlatform.so OVRPlugin.c
+$clang -fdeclspec -gdwarf -fPIC -shared -o out/libOVRPlugin.so libpxr_api.so libPxrPlatform.so OVRPlugin.c
