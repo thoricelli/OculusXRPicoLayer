97a1582d4518ccba4278cfaab83e009dc80dff5f
thoricelli <roanheylen@hotmail.com>
2024-06-11 12:45:34 +0200

    Logical error.

--- src/Logger.c
+++ src/Logger.c
@@ -14,16 +14,16 @@ typedef enum LogFunctionType_ {
 //Logs that the function had been called.
 void LogFunction(LogFunctionType type, CallFrequency frequency, const char func[]) {
     #if VERBOSE
-        #if VERBOSITY_FUNCTIONS > 0 && !FUNCTIONS_ALLOW_SPAM
+        #if VERBOSITY_FUNCTIONS >= 0
         if (type == IMPLEMENTED && frequency == NORMAL)
             __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s has been called!", func);
         #endif
-        #if VERBOSITY_FUNCTIONS <= 0 && !FUNCTIONS_ALLOW_SPAM
+        #if VERBOSITY_FUNCTIONS <= 0
         if (type == NON_IMPLEMENTED && frequency == NORMAL)
             __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s called, but not implemented!", func);
         #endif
 
-        #if VERBOSITY_FUNCTIONS > 0 && FUNCTIONS_ALLOW_SPAM
+        #if VERBOSITY_FUNCTIONS >= 0 && FUNCTIONS_ALLOW_SPAM
             if (type == IMPLEMENTED && frequency == FREQUENT)
                 __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s has been called!", func);
         #endif
