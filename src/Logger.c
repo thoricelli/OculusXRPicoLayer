4a59897a98e97642764ce0cd3464b78885ce036b
thoricelli <roanheylen@hotmail.com>
2024-06-11 12:23:48 +0200

    Implementing the toggle for logging.

--- src/Logger.c
+++ src/Logger.c
@@ -4,21 +4,23 @@
 
 //Logs that the function had been called.
 void LogFunction(LogFunctionType type, CallFrequency frequency, char* func) {
-    #if VERBOSITY_FUNCTIONS > 0 && !FUNCTIONS_ALLOW_SPAM
-    if (type == IMPLEMENTED && frequency == NORMAL)
-        __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s has been called!", func);
-    #endif
-    #if VERBOSITY_FUNCTIONS <= 0 && !FUNCTIONS_ALLOW_SPAM
-    if (type == NON_IMPLEMENTED && frequency == NORMAL)
-        __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s called, but not implemented!", func);
-    #endif
-
-    #if VERBOSITY_FUNCTIONS > 0 && FUNCTIONS_ALLOW_SPAM
-        if (type == IMPLEMENTED && frequency == FREQUENT)
+    #if VERBOSE
+        #if VERBOSITY_FUNCTIONS > 0 && !FUNCTIONS_ALLOW_SPAM
+        if (type == IMPLEMENTED && frequency == NORMAL)
             __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s has been called!", func);
-    #endif
-    #if VERBOSITY_FUNCTIONS <= 0 && FUNCTIONS_ALLOW_SPAM
-        if (type == NON_IMPLEMENTED && frequency == FREQUENT)
+        #endif
+        #if VERBOSITY_FUNCTIONS <= 0 && !FUNCTIONS_ALLOW_SPAM
+        if (type == NON_IMPLEMENTED && frequency == NORMAL)
             __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s called, but not implemented!", func);
+        #endif
+
+        #if VERBOSITY_FUNCTIONS > 0 && FUNCTIONS_ALLOW_SPAM
+            if (type == IMPLEMENTED && frequency == FREQUENT)
+                __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s has been called!", func);
+        #endif
+        #if VERBOSITY_FUNCTIONS <= 0 && FUNCTIONS_ALLOW_SPAM
+            if (type == NON_IMPLEMENTED && frequency == FREQUENT)
+                __android_log_print(ANDROID_LOG_INFO, PLUGIN_NAME, "%s called, but not implemented!", func);
+        #endif
     #endif
 }
